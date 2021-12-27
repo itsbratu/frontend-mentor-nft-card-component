ee6b499605399689d3978c9c26c43a9d1cb72eeb
Ionu <itsbratu@outlook.com>
2021-12-27 12:01:41 +0200

    imported styles and fonts

--- nft-card/src/App.js
+++ nft-card/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="text-5xl font-mono">
-      Hello
+      <h1 className="text-neutral-card-bg font-main-font">dada</h1>
     </div>
   );
 }
--- nft-card/tailwind.config.js
+++ nft-card/tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-soft-blue' : '#8BACDA',
+        'primary-cyan' : '00FFF7',
+        'neutral-main-bg' : '#0D192B',
+        'neutral-card-bg' : '#14253D',
+        'neutral-line' : '#2F415B',
+        'neutral-white' : '#FFFFFF'
+      },
+      fontFamily: {
+        'main-font' : ['Outfit' , 'sans-serif'],
+      },
+      fontWeight: {
+        'light-300' : 300,
+        'medium-400' : 400,
+        'bold-600' : 600,
+      },
+    },
   },
   plugins: [],
 }
